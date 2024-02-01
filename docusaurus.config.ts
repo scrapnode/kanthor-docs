d8a4802658ec991d2204bb13015873c4b8bc8a50
Tuan Nguyen <tuan.nguyen@kanthorlabs.com>
2024-02-01 11:44:37 +0700

    fix: url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,15 +38,13 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/kanthorlabs/kanthor-docs/edit/main/docs/",
+          editUrl: "https://github.com/kanthorlabs/kanthor-docs/edit/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/kanthorlabs/kanthor-docs/edit/main/docs/",
+          editUrl: "https://github.com/kanthorlabs/kanthor-docs/edit/main/",
         },
         theme: {
           customCss: "./src/css/custom.css",
